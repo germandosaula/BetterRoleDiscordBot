099bee2a0f21d51f48b6d87ae883e863aa67f1c7
LaHuella <german.dosaula@gmail.com>
2025-04-10 19:17:28 +0200

    Express for Render HTTP

--- BetterRole.js
+++ BetterRole.js
@@ -90,4 +90,14 @@ const {
   });
   
   client.login(TOKEN);
-  
+  const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Bot is running!");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`🌐 Servidor web escuchando en el puerto ${PORT}`);
+});
